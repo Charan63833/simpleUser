effaf9f49be416e7c54db073a7c6dcd276169e38
Charan63833 <charantej457@gmail.com>
2021-05-06 21:44:50 +0530

    first commit

---
+++ user-name/SampleProject-01-main/user-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user_details (id,username,password,address,phone_No) values (100L,'Charan','Charan@123','Nellore',8888888888L);
+insert into user_details (id,username,password,address,phone_No) values (101L,'Ravi','Ravi@123','Atmakur',9494949494L);
+insert into user_details (id,username,password,address,phone_No) values (102L,'Mahesh','Mahesh@123','Nellore',7539514862L);
+insert into user_details (id,username,password,address,phone_No) values (103L,'Sri','Sri@123','Kuppam',4567891231L);
+insert into user_details (id,username,password,address,phone_No) values (104L,'Kalyan','Kalyan@123','Peta',6781234567L);
