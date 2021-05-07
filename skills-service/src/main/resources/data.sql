5ab842ccf05ef59ac0c4c22c8d593288abac4e8a
Charan63833 <80826628+Charan63833@users.noreply.github.com>
2021-05-07 21:30:24 +0530

    Add files via upload

---
+++ skills-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into skill (id,name,description,level,user_id) values(1001L, 'Java','Java','Beginner',901234L);
+insert into skill (id,name,description,level,user_id) values(1002L, 'HTML','HTML','Intermediate',903456L);
+insert into skill (id,name,description,level,user_id) values(1003L, 'CSS','CSS','Beginner',904567L);
+insert into skill (id,name,description,level,user_id) values(1004L, 'Spring MVC','Spring MVC','Expert',907685L);
+insert into skill (id,name,description,level,user_id) values(1005L, 'Spring Boot','Spring Boot','Intermediate',904568L);
