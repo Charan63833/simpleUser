7d2d8f3d93fa9fc0343f7f1350bbe706501e2737
Charan63833 <80826628+Charan63833@users.noreply.github.com>
2021-05-07 21:33:06 +0530

    Update data.sql

--- skills-service/src/main/resources/data.sql
+++ skills-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into skill (id,name,description,level,user_id) values(1001L, 'Java','Java','Beginner',901234L);
-insert into skill (id,name,description,level,user_id) values(1002L, 'HTML','HTML','Intermediate',903456L);
-insert into skill (id,name,description,level,user_id) values(1003L, 'CSS','CSS','Beginner',904567L);
-insert into skill (id,name,description,level,user_id) values(1004L, 'Spring MVC','Spring MVC','Expert',907685L);
-insert into skill (id,name,description,level,user_id) values(1005L, 'Spring Boot','Spring Boot','Intermediate',904568L);
+insert into skill (id,name,description,level,user_id) values(1001L, 'C','C','Beginner',811234L);
+insert into skill (id,name,description,level,user_id) values(1002L, 'HTML','HTML','Intermediate',823456L);
+insert into skill (id,name,description,level,user_id) values(1003L, 'Java','Java','Beginner',834567L);
+insert into skill (id,name,description,level,user_id) values(1004L, 'Spring MVC','Spring MVC','Expert',847685L);
+insert into skill (id,name,description,level,user_id) values(1005L, 'Spring Boot','Spring Boot','Intermediate',854568L);
